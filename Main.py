cc1976e63bc83bc36b147b6422179a9746d8dc8f
ardeliaraminta <ardelia.araminta@binus.ac.id>
2021-05-14 02:44:01 +0700

    change main file
    graph can be seen, desc is not finished

--- Main.py
+++ Main.py
@@ -1,125 +1,114 @@
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import ( FigureCanvasTkAgg, NavigationToolbar2Tk)
+
+""" Interpolation Computational Mathematics Final Project """ 
+
+import tkinter
+from tkinter import *
+from tkinter import messagebox
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 import tkinter as tk
-from tkinter import ttk
-import numpy as np 
-import matplotlib.pyplot as plt
+import numpy as np
+import tkinter.font as font
 
 
-class Interpolation(tk.Tk):
+class Interpolation(Tk):
+# the main frame 
 
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        tk.Tk.wm_title(self, "Interpolation ")
-        
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand = True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+    def __init__(self):
+        Tk.__init__(self)
+        self._frame = None
+        self.title("Interpolation")
+        self.switchFrame(mainMenu)
+        self.geometry('800x600')
+        self.config(bg = "black")
 
-        self.frames = {}
+#switch in between the frames 
 
-        for F in (mainMenu, Introduction, Langrange, Newton):
+ #to initiate a new frame : 
+ #class Introduction(Frame):
 
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
+    #def __init__(self, master):
+        #Frame.__init__(self, master)
+        #self.config(bg = "black")
 
-        self.show_frame(mainMenu)
-
-    def show_frame(self, cont):
-
-        frame = self.frames[cont]
-        frame.tkraise()
-
-class mainMenu(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Computational Mathematics", font=fixed_font)
-        label['bg'] = 'blue'
-        label['fg'] = 'white'
-        label.pack(pady = 50, padx = 30)
-
-        label2 = tk.Label(self, text = " by Rohan, Raphael, Ardelia")
-        label.pack(pady=10,padx=10)
-        label2.pack(pady=20, padx= 20)
-
-        button = ttk.Button(self, text=" Interpolation",command=lambda: controller.show_frame(Introduction))
-        button.pack()
-
-        button2 = ttk.Button(self, text="Langrange",command=lambda: controller.show_frame(Langrange))
-        button2.pack()
-
-        button3 = ttk.Button(self, text="Newton Forward Interpolation",command=lambda: controller.show_frame(Newton))
-        button3.pack()
+    def switchFrame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
         
 
+#main menu 
+class mainMenu(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.config(bg = "black")
 
-class Introduction(tk.Frame):
+        label = Label(self, text = "Computational Mathematics"\
+                      , bg = "blue", fg = "white")
+        label.pack()
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Introduction : Linear Interpolation", font=fixed_font)
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Main", command=lambda: controller.show_frame(mainMenu))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Langrange",command=lambda: controller.show_frame(Langrange))
+        labelName = Label(self, text = "by Rohan, Raphael, and Ardelia")
+        labelName.pack() 
+       
+        button = Button(self, text = "Introduction", width = 20, command = lambda: master.switchFrame(Introduction))
+        button.pack(padx = 10, pady = 10)
+        button2 = Button(self, text = "Langrange", width = 20, command = lambda: master.switchFrame(Langrange))
         button2.pack()
+        button3 = Button(self, text = "Exit", width = 20, command = self.close)
+        button3.pack(padx = 10, pady = 10)
 
-class Langrange(tk.Frame):
+    def close(self):
+        self.destroy()
+        exit()
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Langrange", font=fixed_font)
-        label.pack(pady=10,padx=10)
+class Introduction(Frame):
 
-        button1 = ttk.Button(self, text="Back to Main",command=lambda: controller.show_frame(mainMenu))
-        button1.pack()
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.config(bg = "black")
 
-        button2 = ttk.Button(self, text="Page One",command=lambda: controller.show_frame(Introduction))
-        button2.pack()
+        #basic langrange 
 
-# basic langrange 
+        x =np.array([0, 20, 40, 60, 80, 100], float)
+        y =np.array([26.0, 48.6, 61.6, 71.2, 74.8, 75.2], float) 
         
-#not yet implemented to tkinter 
-x =np.array([0, 20, 40, 60, 80, 100], float)
-y =np.array([26.0, 48.6, 61.6, 71.2, 74.8, 75.2], float) 
+        xplt = np.linspace(x[0], x[-1])
+        yplt = np.array([], float)
+        
+        for xp in xplt:
+            yp = 0
+            for xi,yi in zip(x,y): #iterator of array x and y 
+                yp += yi * np.prod(( xp - x[x != xi]) / (xi - x[x != xi]))
+            yplt = np.append(yplt,yp)
+            
+            
+        f = Figure(figsize=(5,5), dpi = 100)
+        a = f.add_subplot(111)
+        a.plot(x,y, 'ro', xplt, yplt, 'b-')
+        
+        canvas = FigureCanvasTkAgg(f, self)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+        
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        
+        toolbar.update()
+        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-xplt = np.linspace(x[0], x[-1])
-yplt = np.array([], float)
-
-for xp in xplt:
-    yp = 0
-
-    for xi,yi in zip(x,y): #iterator of array x and y 
-        yp += yi * np.prod(( xp - x[x != xi]) / (xi - x[x != xi]))
-    yplt = np.append(yplt,yp)
-
- 
-
-
-class Newton(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text=" Newton ", font=fixed_font)
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Main",command=lambda: controller.show_frame(mainMenu))
-        button1.pack()
-
-
-        entryN  = ttk.Entry(self)
-        entryN.pack() 
-
-fixed_font= ("Times New Roman", 12)
-
-app = Interpolation()
-app.mainloop()
+        buttonLang = Button(self, text = "MainMenu", width = 500, command = lambda: master.switchFrame(mainMenu))
+        buttonLang.pack()
 
 
+class Langrange(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.config(bg = "black")
+
+          
+
+if __name__ == "__main__":
+    app = Interpolation()
+    app.mainloop()
